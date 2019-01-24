53b0973daedea160c80a29c51428c2ac15b468d1
huidmar <46790473+huidmar@users.noreply.github.com>
2019-01-24 22:10:06 +0800

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,1 +1,21 @@
-Home Page properties
+package pages;
+
+import org.openqa.selenium.By;
+
+public class HomePage {
+
+    public static By loginLink =
+            By.xpath("//li[@class='nav-login']//a[contains(@href,'javascript:;')]");
+
+    public static By loginUserName =
+            By.xpath("//li[@class='nav-login']//a[contains(text(),'taltest2')]");
+  
+    public static By MainArticleHeading =
+            By.xpath("//h2[contains(text(),'Top Stories')]");
+
+    public static By MainArticleImage =
+            By.xpath("//a[@href='/singapore/saf-to-lower-training-tempo-to-better-focus-on-safety-in-wake-of-actor-aloysius-pangs']");
+
+    public static By MainArticleSummary =
+            By.xpath("//a[@class='block-link']");
+}
