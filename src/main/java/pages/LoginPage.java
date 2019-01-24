657b5553fd9827cda551bac36815172d8c812cb2
huidmar <46790473+huidmar@users.noreply.github.com>
2019-01-24 22:10:58 +0800

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,1 +1,15 @@
-Login Page properties
+package pages;
+
+import org.openqa.selenium.By;
+
+public class LoginPage {
+
+    public static By loginIdField =
+            By.xpath("//input[@id='j_username']");
+
+    public static By loginPasswordField =
+            By.xpath("//input[@id='j_password']");
+
+    public static By loginButton =
+            By.xpath("//button[@class='btn']");
+}
